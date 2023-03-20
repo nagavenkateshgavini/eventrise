445cdae95ce7985c239be8ed62b953f25c27e44c
gvssaisumanth <gvssaisumanth24@gmail.com>
2023-03-20 15:34:30 -0400

    new file:   Frontend/.DS_Store 	new file:   Frontend/README.md 	new file:   Frontend/package-lock.json 	new file:   Frontend/package.json     new file:   Frontend/public/.DS_Store 	new file:   Frontend/public/favicon.png 	new file:   Frontend/public/index.html 	new file:   Frontend/public/logo192.png 	new file:   Frontend/public/logo512.png 	new file:   Frontend/public/manifest.json 	new file:   Frontend/public/robots.txt 	new file:   Frontend/src/.DS_Store 	new file:   Frontend/src/App.css 	new file:   Frontend/src/App.js 	new file:   Frontend/src/App.test.js 	new file:   Frontend/src/assets/celebration.jpeg 	new file:   Frontend/src/assets/crackers.jpeg 	new file:   Frontend/src/assets/tickets.jpeg 	new file:   Frontend/src/common/Footer/footer.css 	new file:   Frontend/src/common/Footer/footer.js 	new file:   Frontend/src/common/Home/Home.js 	new file:   Frontend/src/common/carousel/carousel.js 	new file:   Frontend/src/common/header/header.css 	new file:   Frontend/src/common/header/header.js 	new file:   Frontend/src/index.css 	new file:   Frontend/src/index.js 	new file:   Frontend/src/logo.svg 	new file:   Frontend/src/reportWebVitals.js 	new file:   Frontend/src/setupTests.js

---
+++ Frontend/src/App.js
@@ -0,0 +1,16 @@
+import Header from "./common/header/header";
+import "./App.css";
+import Footer from "./common/Footer/footer";
+import { Home } from "./common/Home/Home";
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Home/>
+      
+      <Footer></Footer>
+    </div>
+  );
+}
+
+export default App;
